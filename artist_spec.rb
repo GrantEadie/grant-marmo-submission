28e633549ccf8145af5ddf998ea3bbf60df8ee36
Grant Eadie <grantleadie@gmail.com>
2021-04-02 08:18:56 -0700

    begin arist spec build and first test.

--- artist_spec.rb
+++ artist_spec.rb
@@ -2,5 +2,12 @@ require 'rspec'
 require_relative 'artist'
 
 describe Artist do
-  # do something
+  let(:artist) { Arist.new("Animal Collective")}
+  
+  context 'artist class is instantiated' do 
+    it('will create a uuid for the artist id') do 
+      expect(artist.id.is_a? String).to be true
+    end
+  end
+
 end
