61de837fa2fb6bb61b69061a7c68c75b382a1541
Grant Eadie <grantleadie@gmail.com>
2021-04-02 08:30:56 -0700

    builds song class before next test.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,8 +1,10 @@
 require 'rspec'
 require('artist.rb')
+require('song.rb')
 
 describe Artist do
   let(:artist) { Artist.new("Animal Collective")}
+  let(:song1) { Song.new("Bluish", "Animal Collective", "Experimental")}
   
   context 'artist class is instantiated' do 
     it('will create a uuid for the artist id') do 
@@ -10,4 +12,9 @@ describe Artist do
     end
   end
 
+  context 'when the add_song method is called' do 
+    it('will assign specfied song to artist') do 
+      expect(artist.add_song(song1)).to(eq([song1]))
+    end
+  end
 end
