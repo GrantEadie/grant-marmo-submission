97a30b8b9a46928d46784594e732ba0fab54544b
Grant Eadie <grantleadie@gmail.com>
2021-04-02 08:16:55 -0700

    finalize artist options.

--- artist.rb
+++ artist.rb
@@ -2,4 +2,27 @@ require('uuid')
 
 class Artist
   def initialize(name)
+
+    @id = uuid.generate
+    @name = name
+    @song_count = 0
+    @top_genres = []
+    @genres = []
+    @featured_song_id = ""
+    @songs = []
+  end
+
+  def id 
+    return @id
+  end
+  
+  def details
+    return {
+      name: @name,
+      song_count: @song_count,
+      top_genres: @top_genres, 
+      genres: @genres, 
+      featured_song_id: @featured_song_id,
+      songs: @songs
+    }
 end
