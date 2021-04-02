a10611189f1273a209544fc207f20bd87a7e9e23
Grant Eadie <grantleadie@gmail.com>
2021-04-02 08:37:11 -0700

    successfully passes first test for UUID creation for new song class.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -2,4 +2,11 @@ require 'rspec'
 require('song.rb') 
 
 describe Song do
+  let(:song) { Song.new("My Girls", "Animal Collective", "Experimental")}
+
+  context 'when song class is instantiated' do 
+    it ('will create a uuid for the song id') do 
+      expect(song.id.is_a? String).to be true
+    end
+  end
 end
